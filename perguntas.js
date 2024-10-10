a01e4e961da0fb626c33381b3cde85ec244b2988
Prof-Wolverine <161845570+Prof-Wolverine@users.noreply.github.com>
2024-10-10 10:48:33 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,41 @@
+criaCartao(
+    'Quem sou eu?',
+    '',
+    'Me chamo: Ydvir Silva de Souza, Tenho: 1,74m, Peso: 78kg'
+)
+
+criaCartao(
+    'Minha formação',
+    '1º Grau / 2° Grau / 3º Grau / Pós',
+    'Estudei Fund I: Escola Bela Vista do Paraíso / Fund II e Med: Colégio Est do PR / Fiz Técnico em Processamento de Dados/Lic Mat PUC/PR / Pós EJA Facinter.'
+)
+
+criaCartao(
+    'Cursos Extras',
+    '',
+    '"Datilografia", Programação, Eletrônica, LIBRAS UFPR'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    '',
+    'WIZARD Idiomas / KNN Idiomas: ambos 4 anos'
+)
+
+criaCartao(
+    'Onde Trabalhei?',
+    '',
+    'Como estagiário Proc Dados: Instituto de Adm Pública / Eletrônica Akilenon: Técnico Eletrônica / Programação Base de dados DETRAN-PR / Fiscalização e Auditoria: Detran-PR / Examinador Prático Detran-PR'
+)
+
+criaCartao(
+    'Morei onde?',
+    '',
+    'Curitiba por aprox. 35 anos, Morei no Litoral do Paraná por 8 anos, E agora estamos em Cidade de Pinhão.'
+)
+
+criaCartao(
+    'Trajetória!',
+    '',
+    'Quando adolescente vendedor: vassouras e milho pipoca, Mais tarde no E.M. fui trabalhar no IMAP (Inst. Munic. Adm Pública) em Curitiba, Após, fui para o DETRAN também em Curitiba (RH, COET, COOHA), Ainda no Detran me formei em Lic Matemática PUC/PR, ja na sequencia passei no Concurso e assumi como professor, Pedindo exoneração no Detran/PR, Cá estamos há 20 anos como professor: passando pelos CEEBJA Paulo Freire em Curitba (Rebouças),'
+)
